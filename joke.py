76224c2c1a866d24f80ada862c5312c71f1d5e3a
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-02 09:00:32 +0530

    Create joke.py

---
+++ joke.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# Reddit API endpoint to fetch memes from the "memes" subreddit
+url = "https://www.reddit.com/r/memes/random.json"
+
+user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+# Make a request to the Reddit API
+response = requests.get(url, headers={'User-agent': user_agent})
+
+# Extract the meme URL from the response JSON
+data = json.loads(response.text)[0]['data']['children'][0]['data']
+meme_url = data['url']
+
+# Create a Markdown image link with the meme URL
+markdown = f"![Funny Meme]({meme_url})"
+
+# Write the Markdown to the README.md file
+with open('README.md', 'a') as file:
+    file.write(markdown)
