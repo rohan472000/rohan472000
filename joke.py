f7cfd17523fcdd1bbc6a2bcd9ca000e83931ccc5
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-08 11:31:02 +0530

    Update joke.py

--- joke.py
+++ joke.py
@@ -27,9 +27,6 @@ contents = [line for line in contents if "![Funny Meme]" not in line]
 
 # Write the Markdown to the README.md file
 with open('README.md', 'w') as file:
-    # Add the warning message if it does not exist
-#     if contents[0].strip() != "## Warning: The memes you see here are highly volatile and have a limited lifespan of 5 minutes. So, better hurry up and laugh before they disappear! 😄":
-#         file.write("## Warning: The memes you see here are highly volatile and have a limited lifespan of 5 minutes. So, better hurry up and laugh before they disappear! 😄\n\n")
     file.write(markdown + "\n\n")
     file.writelines(contents)
 # # Add a warning message at the beginning of the contents
