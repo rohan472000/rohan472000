f1c2bd5a3e56488ac1cf0c60eca14d34e5cfc0a2
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-09-07 18:35:04 +0530

    Update joke.py

--- joke.py
+++ joke.py
@@ -4,15 +4,15 @@ import re
 import requests
 import json
 
-# Reddit API endpoint to fetch memes from the "memes" subreddit, can give other api endpoint if you have any.
+# API endpoint to fetch memes from the "memes" subreddi, can give other api endpoint if you have any.
 url = "https://www.reddit.com/r/memes/random.json?limit=1"
 #url = "https://www.reddit.com/r/popular/random.json?limit=1"
 #url = "https://v2.jokeapi.dev/joke/random.json?limit=1"
 user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-# Make a request to the Reddit API
+# It will make a request to the Reddit API
 response = requests.get(url, headers={'User-agent': user_agent})
 
-# Extract the  meme URL from the response JSON
+# Now, Extract the  meme URL from the response JSON
 data = json.loads(response.text)[0]['data']['children'][0]['data']
 meme_url = data['url']
 meme_url = data['url'] + "?width=100&height=100" # Add query string to reduce image size
@@ -34,15 +34,7 @@ with open('README.md', 'r') as file:
 contents = [line for line in contents if "![Funny Meme]" not in line]
 
 
-# Write the Markdown to the README.md file
+# At Last, Write the Markdown to the README.md file
 with open('README.md', 'w') as file:
     file.write(markdown + "\n\n")
     file.writelines(contents)
-# # Add a warning message at the beginning of the contents
-# warning = "##Warning: The memes you see here are highly volatile and have a limited lifespan of 5 minutes. So, better hurry up and laugh before they disappear! 😄\n\n"
-# contents.insert(0, warning)
-
-# # Write the Markdown to the README.md file
-# with open('README.md', 'w') as file:
-#     file.writelines(contents)
-#     file.write(markdown + "\n\n")
