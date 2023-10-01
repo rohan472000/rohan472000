cd966b6d0155590a41a5e2a3a5f4a1cafca0548f
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-10-02 02:57:11 +0530

    fix: Resolve Flake8 linting issues

--- joke.py
+++ joke.py
@@ -15,14 +15,21 @@ IMAGE_EXTENSIONS_PATTERN = re.compile(r'\.(jpg|jpeg|png)$', re.IGNORECASE)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
 def fetch_random_url(api_url, user_agent):
     try:
         response = requests.get(api_url, headers={'User-agent': user_agent})
         response.raise_for_status()
         return response.json()[0]['data']['children'][0]['data']['url']
-    except (requests.exceptions.RequestException, json.JSONDecodeError, KeyError, IndexError) as e:
+    except (
+        requests.exceptions.RequestException,
+        json.JSONDecodeError,
+        KeyError,
+        IndexError
+    ) as e:
         logging.error(f"An error occurred: {e}")
         return None
+
 
 def update_readme_with_url(markdown):
     try:
@@ -39,11 +46,13 @@ def update_readme_with_url(markdown):
     except (IOError, FileNotFoundError) as e:
         logging.error(f"An error occurred: {e}")
 
+
 def main():
     meme_url = fetch_random_url(REDDIT_API_URL, USER_AGENT)
     if meme_url and IMAGE_EXTENSIONS_PATTERN.search(meme_url):
         markdown = f"![Funny Meme]({meme_url}?width=100&height=100)"
         update_readme_with_url(markdown)
+
 
 if __name__ == "__main__":
     main()
