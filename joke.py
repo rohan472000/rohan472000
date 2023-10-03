1701b7277467a895cfd06278d847ab4fc6441aa5
Mahkxai <shr.hardik@gmail.com>
2023-10-02 23:05:04 -0400

    Added GH Action to comment meme on PR

--- joke.py
+++ joke.py
@@ -5,6 +5,8 @@ from typing import Optional
 
 import requests
 
+import os
+
 # Constants
 REDDIT_API_URL = "https://www.reddit.com/r/memes/random.json?limit=1"
 USER_AGENT = (
@@ -17,6 +19,20 @@ IMAGE_EXTENSIONS_PATTERN = re.compile(r"\.(jpg|jpeg|png)$", re.IGNORECASE)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
+def post_comment(pr_url, meme_url, token):
+    """Add meme comment meme on PR."""
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+    comment = {
+        "body": f"![Meme]({meme_url})"
+    }
+    response = requests.post(pr_url, headers=headers, data=json.dumps(comment))
+    if response.status_code != 201:
+        logging.error(f"Failed to post comment: {response.text}")
+        
 
 def fetch_random_url(api_url: str, user_agent: str) -> Optional[str]:
     """Get URL of a random meme from Reddit API."""
@@ -54,11 +70,19 @@ def update_readme_with_url(markdown: str) -> bool:
 
 
 def main() -> None:
-    """Update README with a new meme."""
+    # """Update README with a new meme."""
+    """Add meme comment on PR"""
+    
     meme_url = fetch_random_url(REDDIT_API_URL, USER_AGENT)
     if meme_url and IMAGE_EXTENSIONS_PATTERN.search(meme_url):
-        markdown = f"![Funny Meme]({meme_url}?width=100&height=100)"
-        update_readme_with_url(markdown)
+        # markdown = f"![Funny Meme]({meme_url}?width=100&height=100)"
+        # update_readme_with_url(markdown)
+        token = os.environ.get("GITHUB_TOKEN")
+        pr_url = os.environ.get("PR_URL")
+        if token and pr_url:
+            post_comment(pr_url, meme_url, token)
+        else:
+            logging.error("GITHUB_TOKEN or PR_URL environment variables are missing")
 
 
 if __name__ == "__main__":
