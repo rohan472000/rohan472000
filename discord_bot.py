95e2a55faa93a6086606725fd60f2ecc83679581
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-10-09 22:46:59 +0530

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 import requests
 import logging
 import json
-# import os
+import os
 from collections import deque
 # Constants
 REDDIT_API_URL = "https://www.reddit.com/r/memes/random.json?limit=1"
@@ -17,8 +17,8 @@ USER_AGENT = (
 
 
 # TOKEN = os.environ.get("DISCORD")
-# TOKEN =  os.getenv("DISCORD")
-TOKEN = '${{ secrets.DISCORD }}'
+TOKEN =  os.getenv("DISCORD")
+# TOKEN = '${{ secrets.DISCORD }}'
 # print("token initials are : ", TOKEN)
 
 # Define intents
