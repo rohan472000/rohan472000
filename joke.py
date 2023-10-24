2119b2bfafb56e1714b26f18c927f4a857379f8b
Ahmet Anbar <ahmet.anbar@getir.com>
2023-10-24 23:54:38 +0300

    create reddit service class

--- joke.py
+++ joke.py
@@ -1,35 +1,21 @@
-import json
 import logging
 import re
 from typing import Optional
-import requests
+
+from services.reddit import RedditService
 
 # Constants
-REDDIT_API_URL = "https://www.reddit.com/r/memes/random.json?limit=1"
-USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-)
 README_FILE = "README.md"
 IMAGE_EXTENSIONS_PATTERN = re.compile(r"\.(jpg|jpeg|png)$", re.IGNORECASE)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-def fetch_random_meme(api_url: str, user_agent: str) -> Optional[dict]:
+
+def fetch_random_meme() -> Optional[dict]:
     """Get a random meme data from Reddit API."""
-    try:
-        response = requests.get(api_url, headers={"User-agent": user_agent})
-        response.raise_for_status()
+    return RedditService.get_random_meme()
 
-        if response.status_code < 200 or response.status_code >= 300:
-            logging.error(f"Reddit API request failed with status code {response.status_code}")
-            return None
-
-        return response.json()[0]["data"]["children"][0]["data"]
-    except (requests.exceptions.RequestException, json.JSONDecodeError, KeyError, IndexError) as e:
-        logging.error(f"An error occurred: {e}")
-        return None
 
 def update_readme_with_meme(meme: dict) -> bool:
     """Update README with meme data, returning whether successful."""
@@ -57,11 +43,13 @@ def update_readme_with_meme(meme: dict) -> bool:
         logging.error(f"An error occurred: {e}")
         return False
 
+
 def main() -> None:
     """Update README with a new meme."""
-    meme = fetch_random_meme(REDDIT_API_URL, USER_AGENT)
+    meme = fetch_random_meme()
     if meme and IMAGE_EXTENSIONS_PATTERN.search(meme["url"]):
         update_readme_with_meme(meme)
+
 
 if __name__ == "__main__":
     main()
